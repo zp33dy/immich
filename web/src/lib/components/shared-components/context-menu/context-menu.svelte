f4a776bb815025f8a471baae70ffcec9c29319c4
ben-basten <45583362+ben-basten@users.noreply.github.com>
2024-09-16 07:23:14 -0400

    wip: remove height variable

--- web/src/lib/components/shared-components/context-menu/context-menu.svelte
+++ web/src/lib/components/shared-components/context-menu/context-menu.svelte
@@ -18,15 +18,11 @@
   let left: number;
   let top: number;
 
-  // We need to bind clientHeight since the bounding box may return a height
-  // of zero when starting the 'slide' animation.
-  let height: number;
-
   $: {
     if (menuElement) {
       const rect = menuElement.getBoundingClientRect();
       const directionWidth = direction === 'left' ? rect.width : 0;
-      const menuHeight = Math.min(menuElement.clientHeight, height) || 0;
+      const menuHeight = menuElement.clientHeight || 0;
 
       const calcLeft = Math.min(window.innerWidth - rect.width, x - directionWidth);
       left = Math.max(0, calcLeft);
@@ -36,7 +32,6 @@
 </script>
 
 <div
-  bind:clientHeight={height}
   class="fixed z-10 overflow-hidden rounded-lg duration-[250ms] ease-in {isVisible
     ? 'shadow-lg transition-shadow'
     : 'shadow-none transition-none'}"
